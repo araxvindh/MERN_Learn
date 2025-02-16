128ede451ed8be44c09e3e1fd7a9f710aa18754f
araxvindh <aravindhswamy22@gmail.com>
2025-02-16 22:04:01 +0530

    Day 6 task commit

--- Day6/function.js
+++ Day6/function.js
@@ -1,8 +1,35 @@
+
+
+ // non parameterized function
+
  function testFunction()
  {
     console.log("Function is called"); 
  }
-
- 
  testFunction(); // Function is called  
 
+ // non parameterized return function
+
+ function testFunction1()
+ {
+   reutrn "Function is called";
+ }
+
+ console.log(testFunction1()); // Function is called
+
+ // parameterized function
+
+ function testFunction2(a,b)
+ {
+   console.log(a+b);
+ }
+
+ testFunction2(5,10); // 15
+
+ // parameterized return function
+
+ function testFunction3(a,b) 
+ {
+   return a+b;
+ }
+   console.log(testFunction3(5,10)); // 15
