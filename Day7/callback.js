e1bbec0a33a848b6caf73b380d3b7eb51e6e734b
araxvindh <aravindhswamy22@gmail.com>
2025-02-17 14:51:19 +0530

    React file  has been created

---
+++ Day7/callback.js
@@ -0,0 +1,22 @@
+function formSubmission(message,cbfun)
+{
+    if(cbfun())
+    {
+        console.log(message);
+    }
+    else
+    {
+        console.log("Form submission failed");
+    }
+}
+
+function formValidation()
+{
+    console.log("Form validation is in progress");
+    return true;
+}
+
+formSubmission("Form submitted successfully",formValidation); // Form submitted successfully
+
+
+console.log();
