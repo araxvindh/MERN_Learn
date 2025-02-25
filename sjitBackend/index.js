d64862bc9f1a4e563bdeb53a21ef86d8b22c120c
araxvindh <aravindhswamy22@gmail.com>
2025-02-25 13:48:36 +0530

    Day 12 An Commit

--- sjitBackend/index.js
+++ sjitBackend/index.js
@@ -1,40 +1,39 @@
 const express = require("express");
-const mdb = require("mongoose");
-const dotenv = require("dotenv");
-const Signup = require('./Models/signupSchema');
+const mdb = require("mongoose"); //mdb=>mongoDB
+const dotenv = require("dotenv")
+const signup_Schema = require("./models/signupSchema")
 const app = express();
+app.use(express.json());
 const PORT = 3001;
-
 dotenv.config();
-app.use(express.json()); 
 
 mdb
-  .connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log("MONGODB CONNECTION SUCCESS");
-  })
-  .catch((err) => {
-    console.log("CHECK YOUR CONNECTION", err);
-  });
-
+    .connect(process.env.MONGODB_URL)   //(it can be used another type to block visibilty=>process.env.MONGODB_URL). (localhost if not works use this ip: 127.0.0.1:27017)
+    .then(() => {
+        console.log("MDB Connection Successful");
+    })
+    .catch((err) => {
+        console.log("Check yout Connection", err);
+    });
 
 app.post("/signup", (req, res) => {
-  try {
-    const { firstName, lastName, email, password, phoneNumber } = req.body;
-    const newSignup = new Signup({
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      phoneNumber:phoneNumber ,
-      password: password
-    });
-    newSignup.save();
-    console.log("SIGNUP SUCCESS");
-    ({ message: "SIGNUP DONE", isSignup: true }); 
-  } catch (error) {
-    console.log("ERROR", error);
-    res.status(201).json({ message: "SIGNUP FAILED" ,isSignup:false}); 
-  }
+    try {
+        console.log(req.body);
+        const { firstName, lastName, email, password, phoneNumber } = req.body;
+        const newSignup = new signup_Schema({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            phoneNumber: phoneNumber,
+            password: password
+        });
+        newSignup.save();
+        console.log("SIGNUP SUCCESS");
+        res.status(201).json({ message: "SIGNUP DONE", isSignup: true });
+    } catch (error) {
+        console.log("ERROR", error);
+        res.status(201).json({ message: "SIGNUP FAILED", isSignup: false });
+    }
 });
 
-app.listen(PORT, () => console.log("SERVER STARTED"));
+app.listen(PORT, () => console.log("Server Started Successfully"));
--- sjitBackend/models/signupSchema.js
+++ sjitBackend/models/signupSchema.js
@@ -1,12 +1,13 @@
-const mdb = require('mongoose')
-const signupSchema = mdb.Schema({
-    fisrtName : String,
-    LastName : String,
-    email : String ,
-    Password : String,
-    phoneNumber : Number 
+
+const mdb =require ('mongoose')
+
+
+const signupSchema=mdb.Schema({
+    firstName:String,
+    lastName:String,
+    email:String,
+    password:String,
+    phoneNumber:Number
 })
-
-const signup_Schema = mdb.model("signup",signupSchema);
-
-module.exports = signup_Schema
+const signup_Schema =mdb.model("signups",signupSchema)
+module.exports=signup_Schema
