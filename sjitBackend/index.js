ff979dfc2db34b52592a72d53e3dff0dc40e30c0
araxvindh <aravindhswamy22@gmail.com>
2025-02-25 08:29:17 +0530

    the backend has been added

--- sjitBackend/index.js
+++ sjitBackend/index.js
@@ -7,11 +7,15 @@ const PORT =3001
 app.get("/",(req,res)=>
 {
     res.send("<h1>Welcome to BackEnd Server !!</h1>")
-}
-)
+})
+
+
+app.get("/static",(req,res)=>
+{
+        res.sendFile("D:\\mern\\index.html")
+})
 
 app.listen(PORT,()=>
 {
     console.log("Server Started Successfully")
-}
-)
+})
